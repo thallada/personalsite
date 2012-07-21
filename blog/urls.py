b544ba244344af5bfeb69b94a141fe5801ec88e9
Tyler Hallada <tyler@hallada.net>
2012-07-21 18:45:50 -0400

    Bootstrapping the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('blog.views',
     url(r'^$', 'index', name='index'),
     url(r'^(?P<entry_id>\d+)/$', 'detail', name='detail'),
     url(r'^archive/$', 'archive', name='archive'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- settings.py
+++ settings.py
@@ -5,19 +5,19 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Tyler Hallada', 'tyler@hallada.net'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/thallada/workspace/personalsite/db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/thallada/workspace/personalsite/db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
