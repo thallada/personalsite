f92fe46bfb90e7b9b82cc4dec5e57751d2236c57
Tyler Hallada <tyler@hallada.net>
2012-11-11 18:51:47 -0500

    Commenting and errors work now.

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,12 @@ from django.contrib.comments import Comment
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
-from django.contrib.comments.views import CommentPostBadRequest
+from django.contrib.comments.views.comments import CommentPostBadRequest
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.utils.html import escape
 from django.db import models
 from django.contrib import comments
 from django.contrib.comments import signals
-from django.contrib.comments.views.utils import next_redirect
 import json
 import markdown_deux
 
@@ -43,104 +42,93 @@ def detail(request, entry_id, next=None, using=None):
     e = get_object_or_404(Entry, pk=entry_id)
     c = Comment.objects.filter(object_pk=entry_id)
 
-    # Following code is taken mostly from the django.contrib.comments model,
-    # specifically from the post_comment view.
-    # Fill out some initial data fields from an authenticated user, if present
-    data = request.POST.copy()
-    if request.user.is_authenticated():
-        if not data.get('name', ''):
-            data["name"] = request.user.get_full_name() or request.user.username
-        if not data.get('email', ''):
-            data["email"] = request.user.email
+    if (request.method == 'POST'):
+        # Following code is taken mostly from the django.contrib.comments model,
+        # specifically from the post_comment view.
 
-    # Check to see if the POST data overrides the view's next argument.
-    next = data.get("next", next)
+        # Fill out some initial data fields from an authenticated user, if present
+        data = request.POST.copy()
+        if request.user.is_authenticated():
+            if not data.get('name', ''):
+                data["name"] = request.user.get_full_name() or request.user.username
+            if not data.get('email', ''):
+                data["email"] = request.user.email
 
-    # Look up the object we're trying to comment about
-    ctype = data.get("content_type")
-    object_pk = data.get("object_pk")
-    if ctype is None or object_pk is None:
-        return CommentPostBadRequest("Missing content_type or object_pk field.")
-    try:
-        model = models.get_model(*ctype.split(".", 1))
-        target = model._default_manager.using(using).get(pk=object_pk)
-    except TypeError:
-        return CommentPostBadRequest(
-            "Invalid content_type value: %r" % escape(ctype))
-    except AttributeError:
-        return CommentPostBadRequest(
-            "The given content-type %r does not resolve to a valid model." % \
-                escape(ctype))
-    except ObjectDoesNotExist:
-        return CommentPostBadRequest(
-            "No object matching content-type %r and object PK %r exists." % \
-                (escape(ctype), escape(object_pk)))
-    except (ValueError, ValidationError), e:
-        return CommentPostBadRequest(
-            "Attempting go get content-type %r and object PK %r exists raised %s" % \
-                (escape(ctype), escape(object_pk), e.__class__.__name__))
+        # Check to see if the POST data overrides the view's next argument.
+        #next = data.get("next", next)
 
-    # Do we want to preview the comment?
-    preview = "preview" in data
+        # Look up the object we're trying to comment about
+        ctype = data.get("content_type")
+        object_pk = data.get("object_pk")
+        if ctype is None or object_pk is None:
+            return CommentPostBadRequest("Missing content_type or object_pk field.")
+        try:
+            model = models.get_model(*ctype.split(".", 1))
+            target = model._default_manager.using(using).get(pk=object_pk)
+        except TypeError:
+            return CommentPostBadRequest(
+                "Invalid content_type value: %r" % escape(ctype))
+        except AttributeError:
+            return CommentPostBadRequest(
+                "The given content-type %r does not resolve to a valid model." % \
+                    escape(ctype))
+        except ObjectDoesNotExist:
+            return CommentPostBadRequest(
+                "No object matching content-type %r and object PK %r exists." % \
+                    (escape(ctype), escape(object_pk)))
+        except (ValueError, ValidationError), e:
+            return CommentPostBadRequest(
+                "Attempting go get content-type %r and object PK %r exists raised %s" % \
+                    (escape(ctype), escape(object_pk), e.__class__.__name__))
 
-    # Construct the comment form
-    form = comments.get_form()(target, data=data)
+        # Do we want to preview the comment?
+        #preview = "preview" in data
 
-    # Check security information
-    if form.security_errors():
-        return CommentPostBadRequest(
-            "The comment form failed security verification: %s" % \
-                escape(str(form.security_errors())))
+        # Construct the comment form
+        form = comments.get_form()(target, data=data)
 
-    # If there are errors or if we requested a preview show the comment
-    if form.errors or preview:
-        template_list = [
-            # These first two exist for purely historical reasons.
-            # Django v1.0 and v1.1 allowed the underscore format for
-            # preview templates, so we have to preserve that format.
-            "comments/%s_%s_preview.html" % (model._meta.app_label, model._meta.module_name),
-            "comments/%s_preview.html" % model._meta.app_label,
-            # Now the usual directory based template hierarchy.
-            "comments/%s/%s/preview.html" % (model._meta.app_label, model._meta.module_name),
-            "comments/%s/preview.html" % model._meta.app_label,
-            "comments/preview.html",
-        ]
-        return render_to_response(
-            template_list, {
-                "comment": form.data.get("comment", ""),
-                "form": form,
-                "next": next,
-            },
-            RequestContext(request, {})
+        # Check security information
+        if form.security_errors():
+            return CommentPostBadRequest(
+                "The comment form failed security verification: %s" % \
+                    escape(str(form.security_errors())))
+
+        # If there are errors
+        if form.errors:
+            return render_to_response(
+                'blog/detail.html', {
+                    'comment': form.data.get('comment', ''),
+                    'form': form,
+                    'entry': e,
+                },
+                context_instance=RequestContext(request)
+            )
+
+        # Otherwise create the comment
+        comment = form.get_comment_object()
+        comment.ip_address = request.META.get("REMOTE_ADDR", None)
+        if request.user.is_authenticated():
+            comment.user = request.user
+
+        # Signal that the comment is about to be saved
+        responses = signals.comment_will_be_posted.send(
+            sender  = comment.__class__,
+            comment = comment,
+            request = request
         )
 
-    # Otherwise create the comment
-    comment = form.get_comment_object()
-    comment.ip_address = request.META.get("REMOTE_ADDR", None)
-    if request.user.is_authenticated():
-        comment.user = request.user
+        for (receiver, response) in responses:
+            if response == False:
+                return CommentPostBadRequest(
+                    "comment_will_be_posted receiver %r killed the comment" % receiver.__name__)
 
-    # Signal that the comment is about to be saved
-    responses = signals.comment_will_be_posted.send(
-        sender  = comment.__class__,
-        comment = comment,
-        request = request
-    )
-
-    for (receiver, response) in responses:
-        if response == False:
-            return CommentPostBadRequest(
-                "comment_will_be_posted receiver %r killed the comment" % receiver.__name__)
-
-    # Save the comment and signal that it was saved
-    comment.save()
-    signals.comment_was_posted.send(
-        sender  = comment.__class__,
-        comment = comment,
-        request = request
-    )
-
-    return next_redirect(data, next, comment_done, c=comment._get_pk_val())
+        # Save the comment and signal that it was saved
+        comment.save()
+        signals.comment_was_posted.send(
+            sender  = comment.__class__,
+            comment = comment,
+            request = request
+        )
 
     return render_to_response('blog/detail.html', {'entry': e, 'comments': c,
             'next': e.get_absolute_url()},
