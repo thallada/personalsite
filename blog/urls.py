a55c34ad7a316d0dd7e0e966d8d39a88fa76a64a
Tyler Hallada <tyler@hallada.net>
2012-12-03 21:21:28 -0500

    Adding redirects to resume.pdf using RedirectView.

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from blog.models import EntriesFeed
+from django.views.generic import RedirectView
+from django.conf import settings
 
 urlpatterns = patterns('blog.views',
     url(r'^archive/$', 'archive', name='archive'),
     url(r'^about/$', 'about', name='about'),
     url(r'^rss/$', EntriesFeed(), name='rss'),
     url(r'^projects/$', 'projects', name='projects'),
+    url(r'^resume(.pdf)?/$', RedirectView.as_view(
+            url=settings.STATIC_URL + 'resume.pdf'),
     url(r'^ajax/mardown_comment/$', 'markdown_comment',
             name='markdown_comment'),
     url(r'^ajax/get_comment/$', 'get_comment', name='get_comment'),
